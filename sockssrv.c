89ddb63f2e7b19b6604f2c830c43ab4ca7291c6d
Lars The <lars18th@users.noreply.github.com>
2025-03-05 20:34:56 +0100

    Improve inet mode errors
    More clear errors when using the inet mode.

--- sockssrv.c
+++ sockssrv.c
@@ -307,7 +307,7 @@ int main(int argc, char** argv) {
 		socklen_t peerlen = SOCKADDR_UNION_LENGTH(&c.addr);
 
 		if (getpeername(c.fd, (struct sockaddr *)peer, &peerlen) < 0) {
-			dprintf(2, "error: inetd getpeername()\n");
+			dprintf(2, "error: can't open stdin\n");
 			return 1;
 		}
 
@@ -316,7 +316,7 @@ int main(int argc, char** argv) {
 			copyloop(c.fd, fd);
 			close(fd);
 		} else {
-			return 1; /* quit with error code */
+			return -1; /* quit with error code */
 		}
 
 		return 0;
